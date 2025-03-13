b24211db13356559f29a6b1a8ad56c32304f3018
Chitsanuphong Thongyingmak <ballth146352@gmail.com>
2025-03-13 20:32:50 +0700

    4.0

--- js/script.js
+++ js/script.js
@@ -20,3 +20,32 @@ function openTab(event, tabName) {
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelector(".tab-button").click();
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const titleText = "Welcome to My Portfolio";
+    const descriptionText = "Hi, I'm Ball. I'm a student passionate about technology.";
+    const speed = 50; 
+
+    let titleIndex = 0;
+    let descriptionIndex = 0;
+
+    function typeTitle() {
+        if (titleIndex < titleText.length) {
+            document.getElementById("typewriter-title").textContent += titleText.charAt(titleIndex);
+            titleIndex++;
+            setTimeout(typeTitle, speed);
+        } else {
+            setTimeout(typeDescription, 500); 
+        }
+    }
+
+    function typeDescription() {
+        if (descriptionIndex < descriptionText.length) {
+            document.getElementById("typewriter-text").textContent += descriptionText.charAt(descriptionIndex);
+            descriptionIndex++;
+            setTimeout(typeDescription, speed);
+        }
+    }
+
+    typeTitle(); 
+});
